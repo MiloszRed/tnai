f00f6bacc84a1d842b0b57d11f4e27a5f31f7651
ra-baal <balcerowskirafal@gmail.com>
2022-06-08 16:56:56 +0200

    HomeController redirection

--- TNAI_Blog/TNAI.MVC/Controllers/HomeController.cs
+++ TNAI_Blog/TNAI.MVC/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace TNAI.MVC.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return RedirectToAction("Index", "Posts");
         }
 
         public ActionResult About()
